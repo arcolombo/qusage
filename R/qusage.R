7ff6c8d0bc5127ce1233b9180905547d5500dda3
c.bolen <c.bolen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-05-16 06:08:49 +0000

    Altered the main qusage function to include the n.points parameter. Changed Rdocs accordingly
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/qusage@90397 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/qusage.R
+++ R/qusage.R
@@ -20,7 +20,8 @@ qusage = function(eset,             ##a matrix of log2(expression values), with 
                   geneSets,         ##a list of pathways to be compared. Each item in the list is a vector of names that correspond to the row names of eset.
                   pairVector=NULL,  ##A vector of factors (usually just 1,2,3,etc.) describing the sample pairings. This is often just a vector of patient IDs or something similar. If not provided, all samples are assumed to be independent.
                   var.equal=FALSE,  ##a logical variable indicating whether to treat the two variances as being equal. If TRUE then the pooled variance is used to estimate the variance otherwise the Welch approximation is used.
-                  filter.genes=FALSE ##a boolean indicating whether the genes in eset should be filtered to remove genes with low mean and sd.
+                  filter.genes=FALSE##a boolean indicating whether the genes in eset should be filtered to remove genes with low mean and sd.
+                  n.points=2^12     ##The number of points to sample the convolution at. Passed to aggregateGeneSet
                  ){
   cat("Calculating gene-by-gene comparisons...")
   results = makeComparison(eset, labels, contrast, pairVector=pairVector,var.equal=var.equal)
@@ -31,7 +32,7 @@ qusage = function(eset,             ##a matrix of log2(expression values), with 
   cat("Done.\nAggregating gene data for gene sets.")
   nu = floor(min(results$dof,na.rm=T))
   if(nu<5){cat("\nLow sample size detected. Increasing n.points in aggregateGeneSet.")}
-  results = aggregateGeneSet(results, geneSets, silent=F, n.points=ifelse(nu<5,2^(12+(5-nu)*3),2^12))
+  results = aggregateGeneSet(results, geneSets, silent=F, n.points=n.points))
   cat("Done.\nCalculating variance inflation factors...")
   results = calcVIF(eset, results)
   #cat("Done.\nCalculating homogeneity scores...")
