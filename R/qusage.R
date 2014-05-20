4030a17ad4ecb955690df52cb45b96364bdb84c9
c.bolen <c.bolen@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-05-20 05:47:27 +0000

    fixed bug from previous update
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/qusage@90528 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/qusage.R
+++ R/qusage.R
@@ -14,14 +14,14 @@
 ## Many of the parameters are left out of this function for simplicity, so for greater control
 ## each of the functions must be called separately.
 
-qusage = function(eset,             ##a matrix of log2(expression values), with rows of features and columns of samples. OR an object of class ExpressionSet 
-                  labels,           ##vector of labels representing each column of eset.
-                  contrast,         ##a string describing which of the groups in 'labels' we want to compare. This is usually of the form 'trt-ctrl', where 'trt' and 'ctrl' are groups represented in 'labels'. 
-                  geneSets,         ##a list of pathways to be compared. Each item in the list is a vector of names that correspond to the row names of eset.
-                  pairVector=NULL,  ##A vector of factors (usually just 1,2,3,etc.) describing the sample pairings. This is often just a vector of patient IDs or something similar. If not provided, all samples are assumed to be independent.
-                  var.equal=FALSE,  ##a logical variable indicating whether to treat the two variances as being equal. If TRUE then the pooled variance is used to estimate the variance otherwise the Welch approximation is used.
-                  filter.genes=FALSE##a boolean indicating whether the genes in eset should be filtered to remove genes with low mean and sd.
-                  n.points=2^12     ##The number of points to sample the convolution at. Passed to aggregateGeneSet
+qusage = function(eset,              ##a matrix of log2(expression values), with rows of features and columns of samples. OR an object of class ExpressionSet 
+                  labels,            ##vector of labels representing each column of eset.
+                  contrast,          ##a string describing which of the groups in 'labels' we want to compare. This is usually of the form 'trt-ctrl', where 'trt' and 'ctrl' are groups represented in 'labels'. 
+                  geneSets,          ##a list of pathways to be compared. Each item in the list is a vector of names that correspond to the row names of eset.
+                  pairVector=NULL,   ##A vector of factors (usually just 1,2,3,etc.) describing the sample pairings. This is often just a vector of patient IDs or something similar. If not provided, all samples are assumed to be independent.
+                  var.equal=FALSE,   ##a logical variable indicating whether to treat the two variances as being equal. If TRUE then the pooled variance is used to estimate the variance otherwise the Welch approximation is used.
+                  filter.genes=FALSE,##a boolean indicating whether the genes in eset should be filtered to remove genes with low mean and sd.
+                  n.points=2^12      ##The number of points to sample the convolution at. Passed to aggregateGeneSet
                  ){
   cat("Calculating gene-by-gene comparisons...")
   results = makeComparison(eset, labels, contrast, pairVector=pairVector,var.equal=var.equal)
